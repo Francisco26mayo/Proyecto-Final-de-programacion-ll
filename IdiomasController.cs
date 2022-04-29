a4e66677293c94c5a069b3252e98c03d5230e1a6
Francisco <102198882+Francisco26mayo@users.noreply.github.com>
2022-04-29 14:01:57 -0400

    Add files via upload

---
+++ IdiomasController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BIBLIOTECA;
+
+namespace BIBLIOTECA.Controllers
+{
+    public class IdiomasController : Controller
+    {
+        private bibliotecasEntities db = new bibliotecasEntities();
+
+        // GET: Idiomas
+        public ActionResult Index()
+        {
+            return View(db.Idioma.ToList());
+        }
+
+        // GET: Idiomas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Idioma idioma = db.Idioma.Find(id);
+            if (idioma == null)
+            {
+                return HttpNotFound();
+            }
+            return View(idioma);
+        }
+
+        // GET: Idiomas/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Idiomas/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,idioma1,Descripcion,Estado")] Idioma idioma)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Idioma.Add(idioma);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(idioma);
+        }
+
+        // GET: Idiomas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Idioma idioma = db.Idioma.Find(id);
+            if (idioma == null)
+            {
+                return HttpNotFound();
+            }
+            return View(idioma);
+        }
+
+        // POST: Idiomas/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,idioma1,Descripcion,Estado")] Idioma idioma)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(idioma).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(idioma);
+        }
+
+        // GET: Idiomas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Idioma idioma = db.Idioma.Find(id);
+            if (idioma == null)
+            {
+                return HttpNotFound();
+            }
+            return View(idioma);
+        }
+
+        // POST: Idiomas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Idioma idioma = db.Idioma.Find(id);
+            db.Idioma.Remove(idioma);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
