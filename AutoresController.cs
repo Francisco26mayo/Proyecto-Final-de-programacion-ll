64cd94292e528803f7228c8e58b75b003f93ad8f
Francisco <102198882+Francisco26mayo@users.noreply.github.com>
2022-04-29 13:58:53 -0400

    Add files via upload

---
+++ AutoresController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BIBLIOTECA;
+
+namespace BIBLIOTECA.Controllers
+{
+    public class AutoresController : Controller
+    {
+        private bibliotecasEntities db = new bibliotecasEntities();
+
+        // GET: Autores
+        public ActionResult Index()
+        {
+            return View(db.Autores.ToList());
+        }
+
+        // GET: Autores/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Autores autores = db.Autores.Find(id);
+            if (autores == null)
+            {
+                return HttpNotFound();
+            }
+            return View(autores);
+        }
+
+        // GET: Autores/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Autores/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Nombres,Pais_origen,Idioma_nativo,Estado")] Autores autores)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Autores.Add(autores);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(autores);
+        }
+
+        // GET: Autores/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Autores autores = db.Autores.Find(id);
+            if (autores == null)
+            {
+                return HttpNotFound();
+            }
+            return View(autores);
+        }
+
+        // POST: Autores/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Nombres,Pais_origen,Idioma_nativo,Estado")] Autores autores)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(autores).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(autores);
+        }
+
+        // GET: Autores/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Autores autores = db.Autores.Find(id);
+            if (autores == null)
+            {
+                return HttpNotFound();
+            }
+            return View(autores);
+        }
+
+        // POST: Autores/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Autores autores = db.Autores.Find(id);
+            db.Autores.Remove(autores);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
