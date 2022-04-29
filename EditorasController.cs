4dfa7cc26726a4e4acf715c6b3934992966e5b05
Francisco <102198882+Francisco26mayo@users.noreply.github.com>
2022-04-29 14:00:08 -0400

    Add files via upload

---
+++ EditorasController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BIBLIOTECA;
+
+namespace BIBLIOTECA.Controllers
+{
+    public class EditorasController : Controller
+    {
+        private bibliotecasEntities db = new bibliotecasEntities();
+
+        // GET: Editoras
+        public ActionResult Index()
+        {
+            return View(db.Editora.ToList());
+        }
+
+        // GET: Editoras/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Editora editora = db.Editora.Find(id);
+            if (editora == null)
+            {
+                return HttpNotFound();
+            }
+            return View(editora);
+        }
+
+        // GET: Editoras/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Editoras/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Descripcion_,Estado")] Editora editora)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Editora.Add(editora);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(editora);
+        }
+
+        // GET: Editoras/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Editora editora = db.Editora.Find(id);
+            if (editora == null)
+            {
+                return HttpNotFound();
+            }
+            return View(editora);
+        }
+
+        // POST: Editoras/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Descripcion_,Estado")] Editora editora)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(editora).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(editora);
+        }
+
+        // GET: Editoras/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Editora editora = db.Editora.Find(id);
+            if (editora == null)
+            {
+                return HttpNotFound();
+            }
+            return View(editora);
+        }
+
+        // POST: Editoras/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Editora editora = db.Editora.Find(id);
+            db.Editora.Remove(editora);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
