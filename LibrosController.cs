9d8ad0070eccf36470c40bcd0b4faba303b17c70
Francisco <102198882+Francisco26mayo@users.noreply.github.com>
2022-04-29 14:02:13 -0400

    Add files via upload

---
+++ LibrosController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BIBLIOTECA;
+
+namespace BIBLIOTECA.Controllers
+{
+    public class LibrosController : Controller
+    {
+        private bibliotecasEntities db = new bibliotecasEntities();
+
+        // GET: Libros
+        public ActionResult Index()
+        {
+            return View(db.Libros.ToList());
+        }
+
+        // GET: Libros/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Libros libros = db.Libros.Find(id);
+            if (libros == null)
+            {
+                return HttpNotFound();
+            }
+            return View(libros);
+        }
+
+        // GET: Libros/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Libros/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Descripcion,Signatura_topografica,Isbn,C_Tipo_bibliografia,Autores,editora,año_publicacion,ciencia,Idioma,Estado")] Libros libros)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Libros.Add(libros);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(libros);
+        }
+
+        // GET: Libros/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Libros libros = db.Libros.Find(id);
+            if (libros == null)
+            {
+                return HttpNotFound();
+            }
+            return View(libros);
+        }
+
+        // POST: Libros/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Descripcion,Signatura_topografica,Isbn,C_Tipo_bibliografia,Autores,editora,año_publicacion,ciencia,Idioma,Estado")] Libros libros)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(libros).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(libros);
+        }
+
+        // GET: Libros/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Libros libros = db.Libros.Find(id);
+            if (libros == null)
+            {
+                return HttpNotFound();
+            }
+            return View(libros);
+        }
+
+        // POST: Libros/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Libros libros = db.Libros.Find(id);
+            db.Libros.Remove(libros);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
